e6e41c57fc9e6509a19baa46250f0a9dadc53707
galindo <egalindo54@uabc.edu.mx>
2025-05-29 18:43:04 -0700

    1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -8,58 +8,59 @@ import org.example.plantsql.exeptions.LexicalException;
 import org.example.plantsql.exeptions.SemanticException;
 import org.example.plantsql.exeptions.SyntaxExeption;
 
-import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
 
 public class Main {
     public static void main(String[] args) throws LexicalException, IOException, SyntaxExeption {
+        if (args.length < 1) {
+            System.err.println("Uso: java -jar programa.jar <archivo_entrada> [-o <archivo_salida>]");
+            System.exit(1);
+        }
+
+        String inputFile = args[0];
+        String outputFile = null;
+
+        // Buscar flag -o
+        for (int i = 1; i < args.length - 1; i++) {
+            if (args[i].equals("-o")) {
+                outputFile = args[i + 1];
+                break;
+            }
+        }
+
+        String archivo = readFileFromPath(inputFile);
         SymbolTable symbolTable = new SymbolTable();
 
-        //  ------------------- d
         Lexer lexer = new Lexer();
-        String archivo = readFileFromResources("examples/definition.puml");
-
         lexer.analyze(archivo);
-        Lexer.printTokens(lexer);
 
-
-        // -----------------
-        System.out.println("\n\n\n");
-        System.out.println("**** Analysis sintactico ****");
         Parser parser = new Parser(symbolTable);
         parser.analyze(lexer);
 
-
-        // ----------------
-        System.out.println("\n\n\n");
-        SymbolTable.printSymbolTable(symbolTable);
-
-
-        // -----------
-        System.out.println("\n\n\n");
-        System.out.println("**** Codigo SQl ****");
         Generator generator = new Generator(symbolTable);
         try {
             generator.generate(lexer);
-            System.out.println(generator.getOutput());
-        } catch (SemanticException e) {
-            System.out.println(e);
-        }
+            String output = generator.getOutput();
 
-
-    }
-
-    public static String readFileFromResources(String filename) throws IOException {
-        try (InputStream inputStream = Main.class.getClassLoader().getResourceAsStream(filename)) {
-            if (inputStream == null) {
-                throw new FileNotFoundException("File not found in resources: " + filename);
+            if (outputFile != null) {
+                saveToFile(outputFile, output);
+            } else {
+                System.out.println(output);
             }
-            return new String(inputStream.readAllBytes());
+
+        } catch (SemanticException e) {
+            System.err.println("Error semántico: " + e.getMessage());
         }
     }
 
+    public static String readFileFromPath(String filePath) throws IOException {
+        return new String(Files.readAllBytes(Paths.get(filePath)));
+    }
+
+    public static void saveToFile(String filePath, String content) throws IOException {
+        Files.writeString(Paths.get(filePath), content, StandardOpenOption.CREATE, StandardOpenOption.TRUNCATE_EXISTING);
+    }
 }
